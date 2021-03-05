46c49ddd1e57d9c93b11ca139ccf70f5d74ef229
Pavel Golovanov <publichead@yandex.ru>
2021-03-05 22:16:44 +0300

    add delete route. edit index html

--- app.py
+++ app.py
@@ -70,5 +70,14 @@ def update():
         return redirect(url_for('index'))
 
 
+@app.route('/delete/<id>/', methods=['GET', 'POST'])
+def delete(id):
+    my_data = PhoneBook.query.get(id)
+    db.session.delete(my_data)
+    db.session.commit()
+
+    return redirect(url_for('index'))
+
+
 if __name__ == '__main__':
     app.run(debug=True)
