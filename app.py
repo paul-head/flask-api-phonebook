676c422741103a55167f00edf14307102f5b374b
Pavel Golovanov <publichead@yandex.ru>
2021-03-05 18:27:04 +0300

    * add index and insert routes * refactor index html

--- app.py
+++ app.py
@@ -1,11 +1,14 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for, flash
 from flask_restful import Api, Resource
 from flask_sqlalchemy import SQLAlchemy
 
 
 app = Flask(__name__)
+app.secret_key = "Secret Key"
 api = Api(app)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
 db = SQLAlchemy(app)
 
 
@@ -16,13 +19,41 @@ class PhoneBook(db.Model):
     last_name = db.Column(db.String(100), nullable=False)
     phone_number = db.Column(db.String(), nullable=False)
 
+    def __init__(self, first_name, last_name, phone_number):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.phone_number = phone_number
+
     def __repr__(self):
-        return f"Contact(first_name = {first_name}, last_name = {last_name}, number = {phone_number})"
+        return f"Contact(first_name = {self.first_name}, last_name = {self.last_name}, number = {self.phone_number})"
+
+
+db.create_all()
 
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    contacts = PhoneBook.query.all()
+    return render_template('index.html', contacts=contacts)
+
+
+@app.route('/insert', methods=['POST'])
+def insert():
+    if request.method == 'POST':
+        first_name = request.form['first_name']
+        last_name = request.form['last_name']
+        phone_number = request.form['phone_number']
+
+        mydata = PhoneBook(first_name=first_name,
+                           last_name=last_name,
+                           phone_number=phone_number)
+
+        db.session.add(mydata)
+        db.session.commit()
+
+        # flash("Contact created successfully")
+
+        return redirect(url_for('index'))
 
 
 if __name__ == '__main__':
