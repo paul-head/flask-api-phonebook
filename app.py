9e3074361bc32e09a4d3adb37aaf3e5692e0dbdb
Pavel Golovanov <publichead@yandex.ru>
2021-03-05 15:39:20 +0300

    add phone book model, add base and index templates

--- app.py
+++ app.py
@@ -1,13 +1,27 @@
-from flask import Flask
+from flask import Flask, render_template
+from flask_restful import Api, Resource
+from flask_sqlalchemy import SQLAlchemy
 
 
 app = Flask(__name__)
+api = Api(app)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
+db = SQLAlchemy(app)
+
+
+class PhoneBook(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    first_name = db.Column(db.String(100), nullable=False)
+    last_name = db.Column(db.String(100), nullable=False)
+    phone_number = db.Column(db.String(), nullable=False)
+
+    def __repr__(self):
+        return f"Contact(first_name = {first_name}, last_name = {last_name}, number = {phone_number})"
 
 
 @app.route('/')
 def index():
-    return "hello Flask App"
-
+    return render_template('index.html')
 
 
 if __name__ == '__main__':
